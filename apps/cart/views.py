46a61963f92b6091de303e6fae1caa093c6db041
mybloomwork <mybloomwork@gmail.com>
2020-09-10 02:43:09 +0100

    xperwork app, eight commit

---
+++ apps/cart/views.py
@@ -0,0 +1,47 @@
+from django.conf import settings
+from django.shortcuts import render, redirect
+
+
+from .cart import Cart
+
+def cart_detail(request):
+    cart = Cart(request)
+    productsstring = ''
+
+    for item in cart:
+        product = item['product']
+        url = '/%s/%s/' % (product.category.slug, product.slug)
+        b = "{'id': '%s', 'title': '%s', 'price': '%s', 'quantity': '%s', 'total_price': '%s', 'thumbnail': '%s', 'url': '%s', 'num_available': '%s'}," % (product.id, product.title, product.price, item['quantity'], item['total_price'], product.thumbnail.url, url, product.num_available)
+
+        productsstring = productsstring + b
+
+    context = {
+        'cart': cart,
+        'pub_key': settings.STRIPE_API_KEY_PUBLISHABLE,
+        'productsstring': productsstring.rstrip(',')
+    }
+
+    return render(request, 'cart.html', context)
+
+
+def success(request):
+    cart = Cart(request)
+    cart.clear()
+
+    return render(request, 'success.html')
+
+    productsstring = productsstring + b
+
+    context = {
+        'cart': cart,
+        'pub_key': settings.STRIPE_API_KEY_PUBLISHABLE,
+        'productsstring': productsstring.rstrip(',')
+    }
+
+    return render(request, 'cart.html', context)
+
+def success(request):
+    cart = Cart(request)
+    cart.clear()
+
+    return render(request, 'success.html')
